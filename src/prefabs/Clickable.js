35b2b1a7585a3f9da43122e099b50f35c085499f
Sky Peterson <katherine.ev.peterson@gmail.com>
2021-11-07 19:54:46 -0800

    Changing structure to fit clickable elements and art assets.

--- src/prefabs/Clickable.js
+++ src/prefabs/Clickable.js
@@ -1,14 +1,12 @@
 class Clickable extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, texture){
         super(scene, x, y, texture);
-        // this.visible = false;
-        this.setScale(.15);
-        this.setOrigin(.75,.75);
-        this.setInteractive();
+        this.setInteractive({
+            useHandCursor: true
+        });
         scene.add.existing(this);
         this.on('pointerdown', function(pointer){
             console.log("object clicked");
-            // this.visible = true;
         });
     }
     update(){
